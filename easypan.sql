2ba6197905e321deb1388b8d9685d8c435c2060c
aGhkMjV6 <2703001087@qq.com>
2024-06-25 12:18:22 +0800

    初始化

---
+++ easypan.sql
@@ -0,0 +1,103 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : localhost:3309
+ Source Schema         : easypan
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 16/06/2024 22:48:58
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for email_code
+-- ----------------------------
+DROP TABLE IF EXISTS `email_code`;
+CREATE TABLE `email_code`  (
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '邮箱',
+  `code` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '编号',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `status` tinyint(1) NULL DEFAULT NULL COMMENT '0:未使用  1:已使用',
+  PRIMARY KEY (`email`, `code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '邮箱验证码' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for file_info
+-- ----------------------------
+DROP TABLE IF EXISTS `file_info`;
+CREATE TABLE `file_info`  (
+  `file_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件ID',
+  `user_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户ID',
+  `file_md5` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'md5值，第一次上传记录',
+  `file_pid` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '父级ID',
+  `file_size` bigint(0) NULL DEFAULT NULL COMMENT '文件大小',
+  `file_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `file_cover` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '封面',
+  `file_path` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件路径',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `last_update_time` datetime(0) NULL DEFAULT NULL COMMENT '最后更新时间',
+  `folder_type` tinyint(1) NULL DEFAULT NULL COMMENT '0:文件 1:目录',
+  `file_category` tinyint(1) NULL DEFAULT NULL COMMENT '1:视频 2:音频  3:图片 4:文档 5:其他',
+  `file_type` tinyint(1) NULL DEFAULT NULL COMMENT ' 1:视频 2:音频  3:图片 4:pdf 5:doc 6:excel 7:txt 8:code 9:zip 10:其他',
+  `status` tinyint(1) NULL DEFAULT NULL COMMENT '0:转码中 1转码失败 2:转码成功',
+  `recovery_time` datetime(0) NULL DEFAULT NULL COMMENT '回收站时间',
+  `del_flag` tinyint(1) NULL DEFAULT 2 COMMENT '删除标记 0:删除  1:回收站  2:正常',
+  PRIMARY KEY (`file_id`, `user_id`) USING BTREE,
+  INDEX `idx_create_time`(`create_time`) USING BTREE,
+  INDEX `idx_user_id`(`user_id`) USING BTREE,
+  INDEX `idx_md5`(`file_md5`) USING BTREE,
+  INDEX `idx_file_pid`(`file_pid`) USING BTREE,
+  INDEX `idx_del_flag`(`del_flag`) USING BTREE,
+  INDEX `idx_recovery_time`(`recovery_time`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '文件信息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for file_share
+-- ----------------------------
+DROP TABLE IF EXISTS `file_share`;
+CREATE TABLE `file_share`  (
+  `share_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '分享ID',
+  `file_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件ID',
+  `user_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户ID',
+  `valid_type` tinyint(1) NULL DEFAULT NULL COMMENT '有效期类型 0:1天 1:7天 2:30天 3:永久有效',
+  `expire_time` datetime(0) NULL DEFAULT NULL COMMENT '失效时间',
+  `share_time` datetime(0) NULL DEFAULT NULL COMMENT '分享时间',
+  `code` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '提取码',
+  `show_count` int(0) NULL DEFAULT 0 COMMENT '浏览次数',
+  PRIMARY KEY (`share_id`) USING BTREE,
+  INDEX `idx_file_id`(`file_id`) USING BTREE,
+  INDEX `idx_user_id`(`user_id`) USING BTREE,
+  INDEX `idx_share_time`(`share_time`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '分享信息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `user_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户ID',
+  `nick_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '昵称',
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `qq_open_id` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'qqOpenID',
+  `qq_avatar` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'qq头像',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
+  `join_time` datetime(0) NULL DEFAULT NULL COMMENT '加入时间',
+  `last_login_time` datetime(0) NULL DEFAULT NULL COMMENT '最后登录时间',
+  `status` tinyint(0) NULL DEFAULT NULL COMMENT '0:禁用 1:正常',
+  `use_space` bigint(0) NULL DEFAULT 0 COMMENT '使用空间单位byte',
+  `total_space` bigint(0) NULL DEFAULT NULL COMMENT '总空间',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE INDEX `key_email`(`email`) USING BTREE,
+  UNIQUE INDEX `key_nick_name`(`nick_name`) USING BTREE,
+  UNIQUE INDEX `key_qq_open_id`(`qq_open_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户信息' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
